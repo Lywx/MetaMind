cd8bc50eac1e4ae4b1ed99d2b15d219068c238da
Lywx <linwx_xin@hotmail.com>
2015-01-01 19:14:24 +0800

    Fixed deployment script for Configurations folder copying.

--- Deploy.bat
+++ Deploy.bat
@@ -1,4 +1,4 @@
-robocopy "MetaMind.Acutance\bin\Debug" "C:\Users\Wuxiang\Documents\MetaMind Acutance" /xd "Save" "Data"
-robocopy "MetaMind.PerseveranceHost\bin\Debug" "C:\Users\Wuxiang\Documents\MetaMind Perseverance" /xd "Save" "Data"
+robocopy "MetaMind.Acutance\bin\Debug" "C:\Users\Wuxiang\Documents\MetaMind Acutance" /mir /xd "Save" "Data"
+robocopy "MetaMind.PerseveranceHost\bin\Debug" "C:\Users\Wuxiang\Documents\MetaMind Perseverance" /mir /xd "Save" "Data"
 
 pause
