1e56cc03521dfe7566aaa6bb46844a8b3f4611dd
Wuxiang Lin <linwx_xin@hotmail.com>
2015-06-12 09:41:28 +0800

    [Deployment] new deploy script.

--- Deploy.bat
+++ Deploy.bat
@@ -1,4 +1,3 @@
-robocopy "MetaMind.Acutance\bin\Debug" "C:\Users\Wuxiang\Documents\MetaMind Acutance" /mir /xd "Save" "Data"
-robocopy "MetaMind.Runtime\bin\Debug" "C:\Users\Wuxiang\Documents\MetaMind Runtime" /mir /xd "Save" "Data"
+robocopy "MetaMind.Testimony\bin\Debug" "C:\Users\Wuxiang\Documents\MetaMind" /mir /xd "Save" "Data"
 
 pause
