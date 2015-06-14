cb82d16d450a05cd39e58fd527c19701361ff41f
Wuxiang Lin <linwx_xin@hotmail.com>
2015-06-15 00:57:50 +0800

    [Deploy] Used new F# script to deploy build.

--- Deploy.bat
+++
@@ -1,3 +0,0 @@
-robocopy "MetaMind.Testimony\bin\Debug" "C:\Users\Wuxiang\Documents\MetaMind" /mir /xd "Save" "Data" "app.publish"
-
-pause
---
+++ Deploy.fsx
@@ -0,0 +1,21 @@
+﻿open System
+open System.IO
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let rec CopyDirectory(srcDir : string, desDir : string, excepts : seq<string>) =
+
+    if (not <| Directory.Exists(desDir)) then ignore <| Directory.CreateDirectory(desDir) 
+
+    Directory.GetFiles(srcDir)
+    |> Seq.iter (fun (filePath) -> Path.Combine(desDir, Path.GetFileName(filePath)) |> fun (desPath) -> File.Copy(filePath, desPath, true))
+
+    Directory.GetDirectories(srcDir)
+    |> Seq.filter (fun (folderPath) -> 
+        let folderName = Path.GetFileName(folderPath)
+        not <| (excepts |> Seq.exists ((=) folderName)))
+        
+
+    |> Seq.iter (fun (dirPath) -> Path.Combine(desDir, Path.GetFileName(dirPath)) |> fun (desPath) -> CopyDirectory(dirPath, desPath, excepts))
+
+CopyDirectory(@"MetaMind.Testimony\bin\Debug", @"C:\Users\Wuxiang\Documents\Meta Mind", [ "Data"; "Save"; "app.publish" ])
